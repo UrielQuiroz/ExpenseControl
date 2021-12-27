771719b844fe26bf6cb1e7d5b76afe49290426f0
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-27 13:11:04 -0600

    Validando presupuesto
    Validando que se ingrese un presupuesto valido y de no ser valido mostrara un mensaje de error

--- expense-control/src/components/NuevoPresupuesto.jsx
+++ expense-control/src/components/NuevoPresupuesto.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
+import Mensaje from './Mensaje'
 
 const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+
+    const [ mensaje, setMensaje ] = useState('');
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+        if (!Number(presupuesto) || Number(presupuesto) < 0) {
+            setMensaje('Presupuesto no valido');
+        } else {
+            console.log('Presupuesto valido');
+        }
+    }
+
     return (
         <div className='contenedor-presupuesto contenedor sombra'>
 
-            <form className='formulario' >
+            <form onSubmit={handlePresupuesto} className='formulario' >
                 <div className='campo'>
                     <label>Definir Presupuesto</label>
                     <input type="text"
@@ -15,7 +28,7 @@ const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
                 </div>
 
                 <input type="submit" value="Añadir" />
-
+                {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
             </form>
         </div>
     )
