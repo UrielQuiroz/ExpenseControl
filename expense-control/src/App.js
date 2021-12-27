d735f72d7e48f7eb53fbe648f7e2326f3368c40d
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-27 15:27:06 -0600

    Modal component
    Se creo el componente modal para mostrarlo al dar clic en el botón de "Nuevo", también se le creo la funcionalidad para cerrarlo al dar clic en "X"

--- expense-control/src/App.js
+++ expense-control/src/App.js
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import Header from './components/Header';
+import Modal from './components/Modal';
 import iconNuevoGasto from './img/nuevo-gasto.svg';
 
 function App() {
 
   const [ presupuesto, setPresupuesto ] = useState(0);
   const [ presupuestoValido, setPresupuestoValido ] = useState(false);
+
+  const [ modal, setModal ] = useState(false);
+  const [ animarModal, setAnimarModal ] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+    
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 500);
+    
+  } 
 
   return (
     <div>
@@ -18,9 +31,18 @@ function App() {
 
       {presupuestoValido && (
         <div className="nuevo-gasto">
-          <img src={iconNuevoGasto} alt="icono nuevo gasto" />
+          <img 
+              src={iconNuevoGasto} 
+              alt="icono nuevo gasto"
+              onClick={handleNuevoGasto} />
         </div>
       )}
+
+      {modal && <Modal
+                    setModal={setModal}
+                    animarModal={animarModal}
+                    setAnimarModal={setAnimarModal} /> }
+
     </div>
   );
 }
