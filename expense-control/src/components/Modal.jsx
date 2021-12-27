3acde3542cc030b50f246343512590d1e9c50dda
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-27 15:56:39 -0600

    Formulario
    Se creo el formulario para agregar nuevos gastos

--- expense-control/src/components/Modal.jsx
+++ expense-control/src/components/Modal.jsx
@@ -20,6 +20,37 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
 
             <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
                 <legend>Nuevo Gasto</legend>
+
+                <div className='campo'>
+                    <label htmlFor="nombre">Nombre del Gasto</label>
+                    <input type="text"
+                            id='nombre'
+                            placeholder='Añade el Nombre del Gasto' />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="cantidad">Cantidad</label>
+                    <input type="number"
+                            id='cantidad'
+                            placeholder='Añade la cantidad del Gasto: ej. 300' />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="categoria">Categoria</label>
+                    <select id="categoria">
+                        <option value="">-- SELECCIONE --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="gastos">Gastos en General</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="subscripciones">Subscripciones</option>
+                    </select>
+                </div>
+
+                <input type="submit"
+                        value="Añadir Gasto" />
             </form>
 
         </div>
