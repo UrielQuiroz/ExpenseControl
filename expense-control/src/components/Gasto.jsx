8dac283558f5b9ed3d4bdaaa7ec1f677b74476ad
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-27 18:31:08 -0600

    Mostrando Fecha
    Se creo función para mostrar la fecha en formato dia/mes/año

--- expense-control/src/components/Gasto.jsx
+++ expense-control/src/components/Gasto.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { formatearFecha } from '../helpers'
 
 const Gasto = ({gasto}) => {
-    const { nombre, cantidad, categoria } = gasto;
+    const { nombre, cantidad, categoria, id, fecha } = gasto;
     return (
         <div className='gasto sombra'>
             <div className='contenido-gasto'>
                 <div className='descripcion-gasto'>
                     <p className='categoria'>{gasto.categoria}</p>
                     <p className='nombre-gasto'>{nombre}</p>
+                    <p className='fecha-gasto'>
+                        Agregado el: {''}
+                        <span>{formatearFecha(fecha)}</span>
+                    </p>
                 </div>
             </div>
+
+            <p className='cantidad-gasto'>${cantidad}</p>
+
         </div>
     )
 }
--- expense-control/src/components/ListadoGastos.jsx
+++ expense-control/src/components/ListadoGastos.jsx
@@ -4,7 +4,7 @@ import Gasto from './Gasto';
 const ListadoGastos = ({gastos}) => {
     return (
         <div className='listado-gastos contenedor'>
-            <h2>{gastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
+            <h2>{gastos.length ? 'Gastos Registrados' : 'No hay gastos aún'}</h2>
 
             {gastos.map( gasto => (
                 <Gasto
