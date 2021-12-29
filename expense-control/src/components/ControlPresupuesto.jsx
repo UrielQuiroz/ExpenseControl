044b1abf0c92c9e0bcdc59081c6ae11241abf53b
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-28 18:16:52 -0600

    Apariencia
    Cambiando la apariencia de la aplicación  cuando el presupuesto ya fue agotado

--- expense-control/src/components/ControlPresupuesto.jsx
+++ expense-control/src/components/ControlPresupuesto.jsx
@@ -36,9 +36,9 @@ const ControlPresupuesto = ({ presupuesto, gastos }) => {
             <div>
                 <CircularProgressbar
                         styles={buildStyles({
-                            pathColor: '#3B82F6',
+                            pathColor: porcentaje > 100 ? '#DC2626' : '#3B82F6',
                             trailColor: '#F5F5F5',
-                            textColor: '#3B82F6'
+                            textColor: porcentaje > 100 ? '#DC2626' : '#3B82F6'
                         })}
                         value={porcentaje}
                         text={`${porcentaje}% Gastado`} />
@@ -47,7 +47,7 @@ const ControlPresupuesto = ({ presupuesto, gastos }) => {
 
             <div className='contenido-presupuesto'>
                 <p><span> Presupuesto: </span> {formatearCantidad(presupuesto)} </p>
-                <p><span> Disponible: </span> {formatearCantidad(disponible)} </p>
+                <p className={`${disponible < 0 ? 'negativo' : ''}`}><span> Disponible: </span> {formatearCantidad(disponible)} </p>
                 <p><span> Gastado: </span> {formatearCantidad(gastado)} </p>
             </div>
         </div>
